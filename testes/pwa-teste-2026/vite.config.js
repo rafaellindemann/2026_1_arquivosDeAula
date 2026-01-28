d19f125ead5888dcd219d240cc66f1d50b2c47bc
rafaellindemann <rafaelld@gmail.com>
2026-01-28 20:12:32 -0300

    add teste pwa 2026

---
+++ testes/pwa-teste-2026/vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Teste PWA 2026',
+        short_name: 'Skull',
+        description: 'Um belo teste ',
+        theme_color: '#ff2f70',
+        icons: [
+          {
+            src: 'icon-skull-192.svg',
+            sizes: '192x192',
+            type: 'image/svg',
+          },
+          {
+            src: 'icon-skull-512.svg',
+            sizes: '512x512',
+            type: 'image/svg',
+          },
+        ],
+      },
+    }),
+  ],
+});
