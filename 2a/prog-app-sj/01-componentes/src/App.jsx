c0929ddcfad58e891c396d6083fdda4febb41987
rafaellindemann <rafaelld@gmail.com>
2026-02-20 14:47:16 -0300

    move prog-app-sj here

---
+++ 2a/prog-app-sj/01-componentes/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import DiaDaSemana from './components/DiaDaSemana';
+import Paridade from './components/Paridade';
+import Notas from './components/Notas';
+
+function App() {
+
+  return (
+    <>
+      <h1>01-componentes</h1>
+      <Notas />
+      <Paridade />
+      <DiaDaSemana />
+    </>
+  )
+}
+
+export default App
