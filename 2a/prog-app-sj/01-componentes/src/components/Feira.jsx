8e75500ac0710baeb6e7f3c3f8c63d3d680b978b
rafaellindemann <rafaelld@gmail.com>
2026-02-25 21:19:43 -0300

    add useState

--- 2a/prog-app-sj/01-componentes/src/components/Feira.jsx
+++ 2a/prog-app-sj/01-componentes/src/components/Feira.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react'
+
 function Feira() {
+    const[saida, setSaida] = useState('')
+
+    // let saida = 'erro muito louco'
     function calcularMacas(){
         let preco
         let qtd = Number(prompt("Quantas maças?"))
@@ -9,11 +14,16 @@ function Feira() {
         }
 
         let valor = qtd * preco
-        alert("Valor: R$" + valor.toFixed(2))
+        // alert("Valor: R$" + valor.toFixed(2))
+        // saida = 'muitas maçãs, muitas mesmo'
+        setSaida('R$' + valor.toFixed(2))
     }
     return (
         <div>
             <h2>Feira da maçãs</h2>
+            <p>
+                Resultado: {saida}
+            </p>
             <button onClick={calcularMacas}>Maçãs</button>
         </div>
     )
