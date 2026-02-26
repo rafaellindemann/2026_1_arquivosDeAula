86997acdda302d5c160fda4d069c9a25d8d2d967
rafaellindemann <rafaelld@gmail.com>
2026-02-25 22:14:55 -0300

    add exercício pousada

--- 2a/prog-app-sj/01-componentes/src/App.jsx
+++ 2a/prog-app-sj/01-componentes/src/App.jsx
@@ -4,11 +4,13 @@ import Paridade from './components/Paridade';
 import Notas from './components/Notas';
 import Voto from './components/Voto';
 import Feira from './components/Feira';
+import Pousada from './components/Pousada';
 function App() {
 
   return (
     <>
       <h1>01-componentes</h1>
+      <Pousada />
       <Feira />
       <Voto />
       <Notas />
---
+++ 2a/prog-app-sj/01-componentes/src/components/Pousada.jsx
@@ -0,0 +1,32 @@
+import {useState} from 'react'
+
+function Pousada() {
+    const[conta, setConta] = useState('')
+
+    function calcular(){
+        let diarias = Number(prompt("Quantos dias?"))
+        let valorDiaria
+        if(diarias <= 5){
+            valorDiaria = 100
+        }else if(diarias < 11){
+            valorDiaria = 90
+        }else{
+            valorDiaria = 80
+        }
+        let valorBruto = diarias * valorDiaria
+        let desconto10 = valorBruto * 10/100
+        let desconto15 = valorBruto * 15/100 
+        let valorTotal = valorBruto - desconto10 - desconto15 + 150 
+        setConta("Valor a pagar: R$" + valorTotal.toFixed(2))
+    }
+
+    return (
+        <div className={"container-pousada"}>
+            <h2>Pousada</h2>
+            <button onClick={calcular}>Calcular preço</button>
+            <p>{conta}</p>
+        </div>
+    )
+}
+
+export default Pousada
