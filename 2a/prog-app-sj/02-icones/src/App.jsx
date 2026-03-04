db3e1a98a48f9ea6c3bf49c7ec36f29667802dd7
rafaellindemann <rafaelld@gmail.com>
2026-03-04 20:58:27 -0300

    add ex tabuada e render condicional

--- 2a/prog-app-sj/02-icones/src/App.jsx
+++ 2a/prog-app-sj/02-icones/src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
 import Iconify from './components/Iconify';
+import Tabuada from './components/Tabuada';
+import Render from './components/Render';
+
 
 function App() {
 
   return (
     <>
       <h1>02 - Ícones</h1>
+      <Render />
+      <Tabuada />
       <Iconify />
     </>
   )
