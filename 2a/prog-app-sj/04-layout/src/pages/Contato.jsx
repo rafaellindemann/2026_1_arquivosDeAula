327f39800dbbdcdba566f853d22ba3156aef41b4
rafaellindemann <rafaelld@gmail.com>
2026-04-01 22:29:18 -0300

    att layout e add food park

---
+++ 2a/prog-app-sj/04-layout/src/pages/Contato.jsx
@@ -0,0 +1,10 @@
+import './Paginas.css'
+function Contato() {
+    return (
+        <div className={'cont-contato'}>
+            <h1>Contato</h1>
+        </div>
+    )
+}
+
+export default Contato
---
+++ 2a/prog-app-sj/05-layout-foodPark/src/pages/Produtos.jsx
@@ -0,0 +1,10 @@
+import './Paginas.css'
+function Produtos() {
+    return (
+        <div className={'cont-produtos'}>
+            <h1>Produtos</h1>
+        </div>
+    )
+}
+
+export default Produtos
