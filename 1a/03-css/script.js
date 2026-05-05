2b5ec7485a7d08e17e9d86c43e2fa36de21a34ab
rafaellindemann <rafaelld@gmail.com>
2026-05-04 21:15:04 -0300

    add ex if/elsE  e random

--- 1a/03-css/script.js
+++ 1a/03-css/script.js
@@ -1,5 +1,24 @@
 let resposta = document.getElementById('resultado')
 
+function adivinharNumero(){
+    // let numero = Math.round(Math.random()*10)// 0..10
+    // let numero = Math.floor(Math.random()*10) // 0..9
+    let numero = Math.ceil(Math.random()*3) // 1..10
+    // let numero = Math.random()
+    // console.log(numero);
+    // numero = numero * 10
+    // console.log(numero);
+    // numero = Math.ceil(numero)
+    // console.log(numero);
+
+    let chute = Number(prompt("Chuta aí:"))
+    if(chute == numero){
+        resposta.innerHTML = "Acertou!"
+    }else{
+        resposta.innerHTML = "ERROU!"
+    }
+}
+
 function verificarIdade(){
     // infos
     let idade
