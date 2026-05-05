8ddc23147859a83f913094f6ee2093a304b63304
rafaellindemann <rafaelld@gmail.com>
2026-05-05 19:45:21 -0300

    add dia da semana

--- 1a/03-css/script.js
+++ 1a/03-css/script.js
@@ -1,5 +1,33 @@
 let resposta = document.getElementById('resultado')
 
+function mostrarDiaDaSemana(){
+    // infos
+    let numero, dia
+    // leitura
+    numero = Number(prompt("Digita o número: "))
+    // processamento
+    if(numero == 1){
+        dia = "Domingo"
+    }else if(numero == 2){
+        dia = "Segunda"
+    }else if(numero == 3){
+        dia = "Terça"
+    }else if(numero == 4){
+        dia = "Quarta"
+    }else if(numero == 5){
+        dia = "Quinta"
+    }else if(numero == 6){
+        dia = "Sexta"
+    }else if(numero == 7){
+        dia = "Sábado"
+    }else{
+        dia = "Erro #404, dia não encontrado."
+    }
+    // saídas
+    document.getElementById('resultado').innerHTML = 'Dia escolhido: ' + dia
+
+}
+
 function adivinharNumero(){
     // let numero = Math.round(Math.random()*10)// 0..10
     // let numero = Math.floor(Math.random()*10) // 0..9
