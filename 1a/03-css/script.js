ba24d6b44d87f816fb2093b717d8de8559f38d41
rafaellindemann <rafaelld@gmail.com>
2026-05-07 20:05:08 -0300

    refat d bete

--- 1a/03-css/script.js
+++ 1a/03-css/script.js
@@ -4,15 +4,33 @@ let resposta = document.getElementById('resultado')
 function verificarMeta(){
     // infos
     let totalBruto, premiacoes, presentes, comissoes, lucro
+    let meta
     // entradas
     totalBruto = Number(prompt("Total bruto: "))
     premiacoes = Number(prompt("Premiações: "))
     presentes = Number(prompt("Presentinhos: "))
     comissoes = Number(prompt("Comissões: "))
+    meta = Number(prompt("Meta de hoje:"))
     // processamento
     lucro = totalBruto - premiacoes - presentes - comissoes
+    let mensagem = ''
+    if(lucro >= meta){
+        // bateu a meta - 
+        mensagem = 'Batemos a meta, lucro de R$' + lucro.toFixed(2).replace('.',',')
+    }else{
+        // não bateu a meta
+        if(lucro > 0){
+            // sem meta mas com lucro
+            mensagem = 'Não batemos a meta, mas tivemos lucro de R$' + lucro.toFixed(2).replace('.',',')
+        }else{
+            // sem meta e prejuízo
+            let prejuizo = lucro * -1
+            mensagem = 'Não batemos a meta e ainda tivemos prejuízo de R$' + prejuizo.toFixed(2).replace('.',',')
+        }
+    }
     // saídas
-    resposta.innerHTML = "Lucro de hoje: R$" + lucro.toFixed(2).replace('.',',')
+    resposta.innerHTML = "<br>Lucro de hoje: R$" + lucro.toFixed(2).replace('.',',') + 
+    '<br>' + mensagem
     // let ponto = '.'
     // let virgula = ','
     // resposta.innerHTML = "Lucro de hoje: R$" + 
