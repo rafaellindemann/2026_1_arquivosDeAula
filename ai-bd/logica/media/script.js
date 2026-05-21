11663a4bdd4682883c1c04eae3a0ddd9175f2b5f
rafaellindemann <rafaelld@gmail.com>
2026-05-21 17:51:14 -0300

    add exercícios lógica AI-SJ

---
+++ ai-bd/logica/media/script.js
@@ -0,0 +1,45 @@
+
+function converterRealDolar(){
+    // leitura dos dados
+
+    // processamento
+    
+    // saidas
+    
+}
+
+function calcularMedia(){
+
+    // leitura dos dados
+    let nota1 = Number(prompt("Nota 1:"))
+    let nota2 = Number(prompt("Nota 2:"))
+    
+    // processamento
+    let media = (nota1 + nota2) / 2
+    
+    // saidas
+    console.log(media);
+    alert("Sua média deu :" + media)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
