77fccf84c3bc551731e2a8382b51623739cb7dbd
rafaellindemann <rafaelld@gmail.com>
2026-06-16 20:00:09 -0300

    add rev while

--- 1a/06-repeticao/script.js
+++ 1a/06-repeticao/script.js
@@ -7,13 +7,34 @@
 // }// voltar e testar novamente
 
 
-let peso
+// let peso
 
-let contador = 0
-while(contador < 10){
-    peso = Number(prompt("Digite o peso:"))
-    if(peso>1.1 || peso<0.9){
-        alert("Fora tolerância do peso")
-    }
-    contador++
+// let contador = 0
+// while(contador < 10){
+//     peso = Number(prompt("Digite o peso:"))
+//     if(peso>1.1 || peso<0.9){
+//         alert("Fora tolerância do peso")
+//     }
+//     contador++
+// }
+
+
+// let i = 0 // inicialização
+// while(i < 3){ // cond repet
+    
+//     i++ // contagem
+// }
+
+let total = 0
+
+let i = 0 // inicialização
+while(i < 3){ // cond repet
+    i++ // contagem
+    let preco = Number(prompt("Digite o preço do " + i + "° produto:" ))
+    total = total + preco
+    // total += preco
+
+    document.getElementById('total').innerHTML += i + "° produto: R$" + preco.toFixed(2) + "<br>"
 }
+
+alert(total)
