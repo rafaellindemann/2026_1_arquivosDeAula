03ee96b042f84a86127bf7ad18ece80a0b837559
rafaellindemann <rafaelld@gmail.com>
2026-06-16 22:12:20 -0300

    add vários de repetição

--- 1a/06-repeticao/script.js
+++ 1a/06-repeticao/script.js
@@ -25,16 +25,67 @@
 //     i++ // contagem
 // }
 
-let total = 0
+// let total = 0
 
-let i = 0 // inicialização
-while(i < 3){ // cond repet
-    i++ // contagem
-    let preco = Number(prompt("Digite o preço do " + i + "° produto:" ))
-    total = total + preco
-    // total += preco
+// let i = 0 // inicialização
+// while(i < 3){ // cond repet
+//     i++ // contagem
+//     let preco = Number(prompt("Digite o preço do " + i + "° produto:" ))
+//     total = total + preco
+//     // total += preco
 
-    document.getElementById('total').innerHTML += i + "° produto: R$" + preco.toFixed(2) + "<br>"
-}
+//     document.getElementById('total').innerHTML += i + "° produto: R$" + preco.toFixed(2) + "<br>"
+// }
 
-alert(total)
+// alert(total)
+
+
+
+
+// 📦 Faça um programa que calcule o quadrado de cada número de 1 a 10.
+// let i = 0
+// while(i<10){
+//     i++
+//     let quadrado = i * i
+//     console.log(i + ": " + quadrado);
+    
+// }
+
+// let i = 0
+// let soma = 0
+// while(i < 100){
+//     i++
+//     soma = soma + i
+//     console.log(soma);
+    
+// }
+
+// 🐱‍🏍Mostre os números ímpares de 1 a 20.
+// let i = 0
+// while(i<20){
+//     i++
+//     if(i%2 != 0){
+//         console.log(i);
+//     }  
+// }
+
+// 🐱‍🏍Mostre os números ímpares de 1 a 20.
+// let i = 1
+// while(i<20){
+//     console.log(i);
+//     i += 2
+// }
+
+// 🐱‍🏍Imprima os números de 10 a 1 em ordem decrescente.
+// tem um bug
+// let i = 10 // inicialização
+// // while(i > 0){ // cond repet
+// while(i >= 1){ // cond repet
+//     console.log(i);
+//     i-- // contagem
+// }
+
+
+
+// i%3 == 0
+// i%5 == 0
