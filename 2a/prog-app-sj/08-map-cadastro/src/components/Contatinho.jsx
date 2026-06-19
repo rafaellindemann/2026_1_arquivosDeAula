a96321a81b35a28d2a797764d4a3d1f51d55417f
rafaellindemann <rafaelld@gmail.com>
2026-06-18 22:28:44 -0300

    sync 2a fase

--- 2a/prog-app-sj/08-map-cadastro/src/components/Contatinho.jsx
+++ 2a/prog-app-sj/08-map-cadastro/src/components/Contatinho.jsx
@@ -1,17 +1,44 @@
+import { useState } from 'react'
 import './Contatinho.css'
 
 function Contatinho({c, excluir}) { //*** */
     // console.log(c);
     const {foto, nome, arroba, fone, id } = c //*** */
+    const[editando, setEditando] = useState(false)
+
+    const[inputNome, setInputNome] = useState(nome)
     
   return (
     <div className='cont-contatinho'>
-        <h2>{nome}</h2>
-        <p>{foto}</p>
-        {/* <img src={foto} alt={foto} style={{width: '200px'}} /> */}
-        <img src={foto} alt={foto} className='foto-perfil' />
-        <p>{arroba}</p>
-        <p>{fone}</p>
+        { editando ? 
+          <div>
+            <h2>Nome: <input type="text" value={inputNome} 
+              onChange={(e) => setInputNome(e.target.value)}
+              />
+            </h2>
+            
+            <p>{foto}</p>
+            {/* <img src={foto} alt={foto} style={{width: '200px'}} /> */}
+            <img src={foto} alt={foto} className='foto-perfil' />
+            <input type="text" value={arroba}/>
+            <p>{fone}</p>
+            <button onClick={salvarContatinho}>Salvar</button>
+            {/* <select name="" id="">
+              <option value="" selected></option>
+            </select> */}
+          </div>
+         : 
+          <div>
+            <h2>Nome: {nome}</h2>
+            <p>Foto: {foto}</p>
+            {/* <img src={foto} alt={foto} style={{width: '200px'}} /> */}
+            <img src={foto} alt={foto} className='foto-perfil' />
+            <p>@{arroba}</p>
+            <p>Fone: {fone}</p>
+            <button onClick={() => setEditando(true)}>Editar</button>
+          </div>
+         }
+
         <button onClick={() => excluir(id)}>Excluir</button>
     </div>
   )
