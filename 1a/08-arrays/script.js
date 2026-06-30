3b7130d7ba65963f84bd0790fd8b88bb5e36e281
rafaellindemann <rafaelld@gmail.com>
2026-06-30 19:14:00 -0300

    add array de nomes

---
+++ 1a/08-arrays/script.js
@@ -0,0 +1,19 @@
+
+
+const nomes = [
+  "Lúcio Fernando",
+  "Rivaldo Jesus",
+  "Mônica",
+  "Capitão Ganso",
+  "Peba",
+  "Gill Bates",
+  "Dona Bete",
+  "Kowalski",
+  "Heitor Tuga",
+  "Junin",
+  "Padre Ernan Buco",
+  "GENéZio",
+  "Bilu",
+  "Waldisney",
+  "Tião"
+];
