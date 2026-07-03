64d84b0f5c675f9b5d056c7d9b72e6574eeddaa1
rafaellindemann <rafaelld@gmail.com>
2026-07-03 20:49:48 -0300

    add testes

---
+++ 1a/08-arrays/rev.js
@@ -0,0 +1,23 @@
+let idades = [22, 33, 44, 55, 66, 77, 88, 99, 11]
+
+function rodarDemo(){
+    let valor = Number(prompt("Valor pra remover:"))
+    let pos = idades.indexOf(valor)
+
+
+    // let filtrado = idades.filter((idade) => idade != valor)
+    let idades = idades.filter((idade) => idade > 40)
+
+    console.log(filtrado);
+    
+
+    // if(pos != -1){
+    //     idades.splice(pos, 1)
+    //     alert("Valor removido")
+    //     console.log(idades);
+        
+    // }else{
+    //     alert("Não tem esse valor no array")
+    // }
+}
+
